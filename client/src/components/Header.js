f2449f60dcbf65bb1d52b0b8bd0c6abe28bc8ebb
ed-val2 <e.valbuena.d@gmail.com>
2020-05-24 15:05:40 -0500

    redesign buttons and added functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { searchBarInputChange,  } from '../actions';
-// import Payments from './Payments';
+import { 
+  searchBarInputChange, 
+  hideTopHeadlines, 
+  getNewsByKeyword, 
+  getNewsfeed 
+} from '../actions';
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showSearchBar: false,
-      searchbarText: '',
     };
   }
 
-  _renderContent() {
-    switch (this.props.user) {
-      case null:
-        return;
-      default:
-        return [
-          // <li key="1"><Payments /></li>,
-          // <li key="2" onClick={this._toggleSearchBar.bind(this)} ><a><i className="material-icons left">search</i>Search Pokemon</a></li>,
-          // <li key="2" style={{ margin: '0 30px' }}>Credits: {this.props.user.credits}</li>,
-          <li key="3"><a href="/api/logout">Log Out</a></li>
-        ];
+  _renderHeaderBtns() {
+    if (this.props.userIsSearching) {
+      return [
+        <li key="1" onClick={this._toggleSearchBar.bind(this)} ><a href="#!"><i className="material-icons left">search</i>SEARCH BY KEYWORD</a></li>,
+        <li key="2" onClick={this._onTopStoriesClick.bind(this)} ><a href="#!"><i className="material-icons left">whatshot</i>TOP STORIES</a></li>,
+        <li key="3"><a href="/api/logout">LOG OUT</a></li>
+      ]
+    } else {
+      return [
+        <li key="2" onClick={this._toggleSearchBar.bind(this)} ><a href="#!"><i className="material-icons left">search</i>SEARCH BY KEYWORD</a></li>,
+        <li key="3"><a href="/api/logout">LOG OUT</a></li>
+      ];
     }
+  }
+
+  _onTopStoriesClick() {
+    this.props.hideTopHeadlines(false);
+    this.props.getNewsfeed(1);
   }
 
   _toggleSearchBar() {
@@ -32,20 +41,22 @@ class Header extends Component {
   }
   
   _onSearchBarChange(event) {
-    this.setState({ searchbarText: event.target.value});
+    this.props.searchBarInputChange(event.target.value.toLowerCase())
   }
 
   _handleKeyDown(e) {
     if (e.key === 'Enter') {
+      this.props.hideTopHeadlines(true);
+      this.props.getNewsByKeyword(this.props.searchBarInput, 1);
     }
   }
 
   renderSearchBar() {
     return (
-      <div className="row s12 red darken-4 nav-wrapper">
+      <div className="blue nav-wrapper">
         <form>
           <div className="input-field">
-            <input onKeyDown={this._handleKeyDown.bind(this)} placeholder='Search by ID or name' onChange={this._onSearchBarChange.bind(this)} id="search" type="search" required></input>
+            <input onKeyDown={this._handleKeyDown.bind(this)} placeholder='Search by keyword' onChange={this._onSearchBarChange.bind(this)} id="search" type="search" required></input>
             <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
             <i onClick={this._toggleSearchBar.bind(this)} className="material-icons">close</i>
           </div>
@@ -56,20 +67,21 @@ class Header extends Component {
 
   renderNavigationBar() {
     return (
-      <div className="red darken-4 nav-wrapper">
+      <div className="blue nav-wrapper">
         <Link
           to={this.props.user ? '/surveys' : '/'}
           className="brand-logo"
         >
           <img 
-            src={require("../assets/news_logo.png")} 
+            style={{ marginLeft: 15 }}
+            src={require("../assets/news_logo_white.png")} 
             alt={''} 
             height="55" 
             width="55">
           </img>
         </Link>
         <ul className="right hide-on-med-and-down">
-          {this._renderContent()}
+          {this._renderHeaderBtns()}
         </ul>
       </div>
     );
@@ -91,10 +103,14 @@ class Header extends Component {
 const mapStateToProps = ({ auth, header }) => {
   return {
     user: auth.user,
-    searchBarInput: header.searchBarInput
+    searchBarInput: header.searchBarInput,
+    userIsSearching: header.userIsSearching,
   };
 };
 
 export default connect(mapStateToProps, {
   searchBarInputChange,
+  hideTopHeadlines,
+  getNewsByKeyword,
+  getNewsfeed,
 })(Header);
