c0a1d8d1edb440561d9188bfc185d94366bddc1c
ed-val2 <e.valbuena.d@gmail.com>
2020-05-24 15:04:33 -0500

    new actions to handle searches

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -28,7 +28,68 @@ export const landingIsActive = (value) => {
   return { type: 'LANDING_IS_ACTIVE', payload: value };
 };
 
+export const hideTopHeadlines = (value) => {
+  return { type: 'USER_IS_SEARCHING', payload: value };
+};
+
 export const searchBarInputChange = (input) => {
   return { type: 'CHANGE_USER_SB_INPUT', payload: input };
 };
 
+export const resetError = () => {
+  return { type: 'RESET_ERROR' };
+};
+
+export const getNewsByKeyword = (keyword, page) => async dispatch => {
+  dispatch({ type: 'GET_NEWS_BY_KEYWORD' });
+  try {
+    const res = await API.getNewsByKeyword(keyword, page);
+    if (res.error) {
+      dispatch({ 
+        type: 'GET_NEWS_BY_KEYWORD_FAIL', 
+        payload: res.errorMsg
+      });
+      return;
+    }
+
+    dispatch({ 
+      type: 'GET_NEWS_BY_KEYWORD_SUCCESS', 
+      payload: {
+        news: res.news,
+        totalResults: res.totalResults,
+        paginatorValues: res.paginatorValues,
+      }
+    });
+
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: 'GET_NEWS_BY_KEYWORD_FAIL'});
+  }
+};
+
+export const getNewsfeed = (page) => async dispatch => {
+  dispatch({ type: 'GET_NEWSFEED' });
+  try {
+    const res = await API.getNewsfeed(page);
+
+    if (res.error) {
+      dispatch({ 
+        type: 'GET_NEWSFEED_FAIL', 
+        payload: res.errorMsg
+      });
+      return;
+    }
+
+    dispatch({ 
+      type: 'GET_NEWSFEED_SUCCESS', 
+      payload: {
+        news: res.news,
+        totalResults: res.totalResults,
+        paginatorValues: res.paginatorValues,
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: 'GET_NEWSFEED_FAIL'});
+  }
+};
