525853a5857368b0a2259833ba7d15859db89125
ed-val2 <e.valbuena.d@gmail.com>
2020-05-24 15:06:59 -0500

    added new reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import headerReducer from './headerReducer';
+import homeReducer from './homeReducer';
 
 export default combineReducers({
   auth: authReducer,
   header: headerReducer,
+  home: homeReducer,
 });
